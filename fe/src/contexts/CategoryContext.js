40c9ea1ef0ac0b3111734d5576b7fed29283f684
TurbatBay <learnwithturu@gmail.com>
2023-03-16 12:30:55 +0800

    admin panel BE tei boloh gj uzew

--- fe/src/contexts/CategoryContext.js
+++ fe/src/contexts/CategoryContext.js
@@ -1,16 +1,15 @@
-import { createContext, useState } from "react";
+import { createContext, useState } from 'react'
 
-const CategoryContext = createContext(null);
-
+const CategoryContext = createContext(null)
 const CategoryContextProvider = ({ children }) => {
-  const [categories, setCategories] = useState([]);
-  const URL = "http://localhost:8080/categories";
+  const [categories, setCategories] = useState([])
+  const URL = 'http://localhost:8282/admin/categories'
 
   return (
     <CategoryContext.Provider value={[categories, setCategories, URL]}>
       {children}
     </CategoryContext.Provider>
-  );
-};
+  )
+}
 
-export { CategoryContext, CategoryContextProvider };
+export { CategoryContext, CategoryContextProvider }
